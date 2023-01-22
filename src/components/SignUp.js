c9c67b47c0c31f52a4bf57b077873f4101b20de5
Mohamad Amin Vard <vard00780@gmail.com>
2023-01-22 04:23:50 +0330

    validate added

---
+++ src/components/SignUp.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { validate } from "./validate";
+
+const SignUp = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    isAccepted: false,
+  });
+
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    setErrors(validate(data));
+    console.log(errors);
+  }, [data]);
+
+  const changeHandler = (event) => {
+    if (event.target.name === "isAccepted") {
+      setData({ ...data, [event.target.name]: event.target.checked });
+    } else {
+      setData({ ...data, [event.target.name]: event.target.value });
+    }
+
+  };
+
+  return (
+    <div>
+      <form>
+        <h2>Signup</h2>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            value={data.name}
+            onChange={changeHandler}
+          ></input>
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            value={data.email}
+            onChange={changeHandler}
+          ></input>
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={changeHandler}
+          ></input>
+        </div>
+        <div>
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={data.confirmPassword}
+            onChange={changeHandler}
+          ></input>
+        </div>
+        <div>
+          <label>I accept terms of privacy policy</label>
+          <input
+            type="checkbox"
+            name="isAccepted"
+            value={data.isAccepted}
+            onChange={changeHandler}
+          ></input>
+        </div>
+        <div>
+          <a href="#">Login</a>
+          <button type="submit">Sign Up</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
